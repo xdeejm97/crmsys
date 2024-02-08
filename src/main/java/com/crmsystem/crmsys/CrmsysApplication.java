81f8ccb3189b75aed9cf776583381d49099014c2
xdeejm97 <d.niesmialowski@gmail.com>
2024-02-08 19:49:21 +0100

    First commit

---
+++ src/main/java/com/crmsystem/crmsys/CrmsysApplication.java
@@ -0,0 +1,13 @@
+package com.crmsystem.crmsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmsysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmsysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crmsystem/crmsys/CrmsysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crmsystem.crmsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmsysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
